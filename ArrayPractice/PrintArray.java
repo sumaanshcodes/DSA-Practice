0689ba4527025dc091921ed70c4c799bb2ce9218
Sumansh Yadav <sumanshyadav.1807@gmail.com>
2026-03-29 08:25:12 +0000

    Java dsa Practice

---
+++ ArrayPractice/PrintArray.java
@@ -0,0 +1,8 @@
+public class PrintArray{
+    public static void main(String[] args){
+        int[] arr={2,58,47,59,78,65};
+        for(int i=0;i<=5;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
